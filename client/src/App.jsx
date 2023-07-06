de658511f0b553bd49ac11e25ca07b5a27302a06
MaxiV95 <maximilianovanmegroot@gmail.com>
2023-07-06 02:07:46 -0300

    main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
-function App() {
+import style from "./App.module.css";
 
-  return (
-    <div>
-      Hello worldF
-    </div>
-  )
+import { useLocation, Route, Routes } from "react-router-dom";
+
+function App() {
+	const location = useLocation();
+
+	return (
+		<div className={style.App}>
+      Hello world
+			{/* {location.pathname !== "/" && <NavBar />} */}
+			<Routes>
+				{/* <Route path="/" element={<Landing />} /> */}
+				{/* <Route path="/home" element={<Home />} /> */}
+				{/* <Route path="/form" element={<Form />} /> */}
+				{/* <Route path="/detail/:id" element={<Detail />} /> */}
+				{/* <Route path="/about" element={<About />} /> */}
+				{/* <Route path="/upload/:id" element={<Upload />} /> */}
+				{/* <Route path="*" element={<NotFound />} /> */}
+			</Routes>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+// import { store } from "./redux/store";
+// import { Provider } from "react-redux";
 import App from './App.jsx'
-import './index.css'
+import axios from "axios";
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL_SERVER;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+		{/* <Provider store={store}> */}
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		{/* </Provider> */}
+	</React.StrictMode>
 )
