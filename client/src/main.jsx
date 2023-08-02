e556096d3cc8f9796b1c63bb1c3e001650da3af2
MaxiV95 <maximilianovanmegroot@gmail.com>
2023-08-02 03:45:22 -0300

    optimized

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { store } from "./redux/store";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import axios from "axios";
-
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL_SERVER;
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
